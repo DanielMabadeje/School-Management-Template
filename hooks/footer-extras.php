12087a29bfb75348c96338e46dfff16f7e404eea
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-06-06 09:24:39 -0700

    addd files

---
+++ hooks/footer-extras.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 1.1.0
+    </div>
+    <strong>Student Management &nbsp;<a href="https://api.whatsapp.com/send?phone=+2348153106597">Developed By Daniel Mabadeje</a>.</strong> All rights
+    reserved.
+  </footer>
